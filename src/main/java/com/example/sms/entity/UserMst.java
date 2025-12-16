c15e807d09704212beda5df7839ec969083de374
oOneseok <sok1646@naver.com>
2025-12-16 15:12:14 +0900

    commit

---
+++ src/main/java/com/example/sms/entity/UserMst.java
@@ -0,0 +1,23 @@
+package com.example.sms.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "TB_USERMST")
+public class UserMst {
+
+    @Id
+    @Column(name = "USER_ID", length = 10)
+    private String userId;
+
+    @Column(name = "USER_NM", length = 50, nullable = false)
+    private String userNm;
+
+    @Column(name = "PSWD", length = 20, nullable = false)
+    private String pswd;
+}
